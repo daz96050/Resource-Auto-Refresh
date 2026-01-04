4b2a89e72af7ab7b0d98c2697f3d55591adc8617
DAZ96050 <daz96050.gitub@gmail.com>
2026-01-03 19:20:32 -0600

    add exclusions list and fork repo

--- control.lua
+++ control.lua
@@ -1,14 +1,46 @@
+
+local function get_excluded_resources()
+	local setting = settings.global["resource-refresh-exclusions"]
+	if not setting or not setting.value or setting.value == "" then
+		return {}
+	end
+
+	local excluded = {}
+	for name in string.gmatch(setting.value, "[^,%s]+") do
+		excluded[name] = true
+	end
+	return excluded
+end
+
+local function rebuild_exclusions()
+	storage.excluded_resources = get_excluded_resources()
+end
+
+script.on_init(rebuild_exclusions)
+script.on_configuration_changed(rebuild_exclusions)
+
+script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+	if event.setting == "resource-refresh-exclusions" then
+		rebuild_exclusions()
+	end
+end)
+
+
 script.on_event(defines.events.on_resource_depleted, function(event)
+	local entity = event.entity
+	if not (entity and entity.valid) then return end
+
+	if storage.excluded_resources and storage.excluded_resources[entity.name] then
+		return
+	end
 	if storage.generator == nil then
 		storage.generator = game.create_random_generator()
   	end
-
-	local first_player = game.get_player(1)
-	local entity  = event.entity
+	
 	local surface = entity.surface
 	local x = entity.position.x
 	local y = entity.position.y
-	local resourcetype = "stone"
+	local resourcetype = entity.name
 	local resourceamount = 100
 
 	
@@ -42,11 +74,10 @@ script.on_event(defines.events.on_resource_depleted, function(event)
 	}
 
 	if #mines > 0 then
-		for i, v in pairs(mines) do
+		for _, v in pairs(mines) do
 			v.update_connections()
 		end
 	end
-
 end)
 
 
---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "string-setting",
+        name = "resource-refresh-exclusions",
+        setting_type = "runtime-global",
+        default_value = "",
+        allow_blank = true,
+        order = "a"
+    }
+})
